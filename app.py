5bedd6a3bfbdfb7e96ac6fa50c1b05bb684a3817
Milo <camilo13777@gmail.com>
2025-07-04 00:34:26 -0300

    docs, feat: gerado arquivo requirements.txt e gerado dataframe.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import kagglehub
+import pandas as pd
+
 
 # Download latest version
 path = kagglehub.dataset_download("Cornell-University/arxiv")
 
 print("Path to dataset files:", path)
+
+
+df = pd.read_json(path +  "/arxiv-metadata-oai-snapshot.json", chunksize=1000, lines=True)
+
